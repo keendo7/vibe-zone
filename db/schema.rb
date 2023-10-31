a11df8d4a575f67b248a13cad89bd281094fe3c0
keendo <m.szczupak3@gmail.com>
2023-10-31 15:32:49 +0100

    add post controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_193053) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_135444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "content"
+    t.bigint "author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
