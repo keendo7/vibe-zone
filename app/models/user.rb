6d65dff340ec4ed85287d8ac00773155ff4326de
keendo <m.szczupak3@gmail.com>
2023-11-02 23:26:41 +0100

    add friendship logic to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ApplicationRecord
   has_many :created_comments, foreign_key: 'commenter_id', class_name: 'Comment'
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
+  has_many :received_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :received_friends, through: :received_friendships, source: 'user'
+  
+  def active_friends
+    friends.select{ |friend| friend.friends.include?(self) }  
+  end
+  
+  def pending_friends
+    friends.select{ |friend| !friend.friends.include?(self) }  
+  end
 end
