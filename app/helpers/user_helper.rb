a0837a512bee2f92f17a6d57ec3ae9368fa3c5f0
keendo <m.szczupak3@gmail.com>
2024-01-06 21:39:50 +0100

    add image processing, modify avatar for user

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,7 @@
 module UserHelper
-  def user_avatar(user)
+  def user_avatar(user, size=50)
     if user.avatar.attached?
-      user.avatar
+      user.avatar.variant(resize_to_fill: [size, nil])
     else
       user.gravatar_url
     end
