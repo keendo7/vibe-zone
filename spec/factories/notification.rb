8efa36500574f1c7613ce599974447d100ca3d9b
keendo <m.szczupak3@gmail.com>
2025-02-05 00:07:00 +0100

    add specs for cleanup notifications job

--- spec/factories/notification.rb
+++ spec/factories/notification.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :notification do
     association :user, factory: :user
     association :sender, factory: :user
+    was_read { false }
 
     trait :for_comment do
       association :notifiable, factory: :comment
@@ -13,6 +14,10 @@ FactoryBot.define do
 
     trait :for_like do
       association :notifiable, factory: :like
+    end
+
+    trait :was_read do
+      was_read { true }
     end
   end
 end
--- spec/jobs/cleanup_notifications_job_spec.rb
+++ spec/jobs/cleanup_notifications_job_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CleanupNotificationsJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  Sidekiq::Testing.fake! do
+    describe '#perform' do
+      it 'enqueues cleanup notifications job' do
+        described_class.perform_async()
+        expect(described_class.jobs.size).to eq(1)
+      end
+    end
+  end
 end
