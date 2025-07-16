0f9e80455676a89fe78ca17ae47986b60543023a
keendo <m.szczupak3@gmail.com>
2025-07-16 22:19:47 +0200

    update loading button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :turbo_frame_request_variant
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
 
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password, :avatar)}
-  end
-
-  def turbo_frame_request_variant
-    request.variant = :turbo_frame if turbo_frame_request?
   end
 end
