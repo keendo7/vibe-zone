68a7b164309dabd4cb6fc14274f85e52b968e543
keendo <m.szczupak3@gmail.com>
2025-07-21 20:24:44 +0200

    modified layout for user's friendship view and partial, added empty state, added pagination

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -3,9 +3,9 @@ class UserFriendshipsController < FriendshipsController
     @user = User.friendly.find(params[:user_id])
    
     if params[:query].present?
-      @friendships = @user.active_friendships.search_friend(params[:query])
+      @pagy, @friendships = pagy_countless(@user.active_friendships.search_friend(params[:query]), items: 15)
     else
-      @friendships = @user.active_friendships
+      @pagy, @friendships = pagy_countless(@user.active_friendships, items: 15)
     end
   end
 
