1346d77514f3b7169ef05036a6f5e3c95669cff7
keendo <m.szczupak3@gmail.com>
2025-08-10 23:26:15 +0200

    fix rubocop errors for like model, update like model spec, add unique index to likes, add error message to locale

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe Like, type: :model do
   end
 
   describe 'validations' do
-     it { is_expected.to validate_uniqueness_of(:user_id).scoped_to([:likeable_id, :likeable_type]).with_message('can only like this item once') }
+    it {
+      expect(subject).to validate_uniqueness_of(:user_id)
+        .scoped_to([:likeable_id, :likeable_type])
+        .with_message('can only like this item once.')
+    }
   end
 end
