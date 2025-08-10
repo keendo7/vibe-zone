cb736e1ced78b40e3c0d076b6d7b7936f18ec5cd
keendo <m.szczupak3@gmail.com>
2025-08-10 23:19:12 +0200

    fix rubocop errors for friendship model, update friendship model spec, add unique index to friendship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_14_165758) do
+ActiveRecord::Schema[7.0].define(version: 2025_08_10_211537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_14_165758) do
     t.bigint "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  let(:user1) { create :user }
-  let(:user2) { create :user }
+  let(:user) { create :user }
+  let(:friend) { create :user }
 
   describe 'relationships' do
     it { is_expected.to belong_to(:user) }
@@ -12,30 +12,32 @@ RSpec.describe Friendship, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:friend_id) }
-    it { is_expected.to validate_presence_of(:user_id) }
-    it { is_expected.to validate_presence_of(:friend_id) }
   end
 
   describe 'validate uniqueness of user and friend' do
     context 'when user is equal friend' do
-      let(:friendship) { build(:friendship, user: user1, friend: user1) }
+      let(:friendship) { build(:friendship, user: user, friend: user) }
+
       it { expect(friendship).not_to be_valid }
     end
 
     context 'when user id not equal friend' do
-      let(:friendship) { build(:friendship, user: user1, friend: user2) }
+      let(:friendship) { build(:friendship, user: user, friend: friend) }
+
       it { expect(friendship).to be_valid }
     end
   end
 
   describe '#is_mutual?' do
     context 'when friendship is not mutual' do
-      let(:friendship) { create(:friendship, user: user1, friend: user2) }
+      let(:friendship) { create(:friendship, user: user, friend: friend) }
+
       it { expect(friendship.is_mutual?).to be false }
     end
 
     context 'when friendship is mutual' do
-      let(:friendship) { create(:friendship, :for_mutual, user: user1, friend: user2) }
+      let(:friendship) { create(:friendship, :for_mutual, user: user, friend: friend) }
+
       it { expect(friendship.is_mutual?).to be true }
     end
   end
